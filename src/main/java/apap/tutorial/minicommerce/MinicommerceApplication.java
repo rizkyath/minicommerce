f60584859cd54e224f157958a33ffa0cb6c2eda8
muhammad erlangga <erlanggamuhammad01@gmail.com>
2021-11-28 22:37:56 +0700

    chore: init project mcommerce

---
+++ src/main/java/apap/tutorial/minicommerce/MinicommerceApplication.java
@@ -0,0 +1,13 @@
+package apap.tutorial.minicommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinicommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinicommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apap/tutorial/minicommerce/MinicommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tutorial.minicommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinicommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
