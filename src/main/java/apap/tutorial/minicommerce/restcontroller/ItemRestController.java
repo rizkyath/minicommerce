de00d5f896011c257391b45078128361a3bfc387
muhammad erlangga <erlanggamuhammad01@gmail.com>
2021-11-29 07:02:51 +0700

    refactor: remove unused import

--- src/main/java/apap/tutorial/minicommerce/restcontroller/ItemRestController.java
+++ src/main/java/apap/tutorial/minicommerce/restcontroller/ItemRestController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.NoSuchElementException;
-
 import javax.validation.Valid;
 import java.text.ParseException;
 
